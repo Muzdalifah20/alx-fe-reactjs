52963e693435d521e3ba4f29338cdaa524c6c042
Muzdalifah <muzdalifahsuliman@gmail.com>
2026-01-11 13:18:22 +0200

    using porps

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,16 +1,18 @@
 import "./App.css";
 // import WelcomeMessage from "./components/WelcomeMessage";
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
-import Footer from "./components/Footer";
-
+// import Header from "./components/Header";
+// import MainContent from "./components/MainContent";
+// import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
 function App() {
   return (
     <>
       {/* <WelcomeMessage /> */}
-      <Header />
+      {/* <Header />
       <MainContent />
-      <Footer />
+      <Footer /> */}
+
+      <UserProfile name="Zozo" age="25" bio="Love color and nature" />
     </>
   );
 }
