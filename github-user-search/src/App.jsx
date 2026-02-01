a5312adfd0c97382813b2fbde82a9cb2aa0dab5e
Muzdalifah <muzdalifahsuliman@gmail.com>
2026-02-01 11:58:12 +0200

    working with api

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <div className="app"></div>
+    </Router>
+  );
+}
+
+export default App;
