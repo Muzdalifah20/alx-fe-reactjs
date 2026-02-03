06f57f21ce5980ba4f8a3b8a8cf77160d28f2bba
Muzdalifah <muzdalifahsuliman@gmail.com>
2026-02-03 12:55:56 +0200

    practicing tailwind with react

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,13 +1,13 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div className="user-profile bg-gray-100 ms:p-6 md:p-8 sm:max-w-sm md:max-w-xs mx-auto my-20 rounded-lg shadow-lg">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full w-36 h-36 mx-auto"
+        className="rounded-full ms:w-24 md:w-36 ms:h-24 md:h-36 mx-auto"
       />
-      <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-      <p className="text-gray-600 text-base">
+      <h1 className="ms:text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
+      <p className="text-gray-600 md:text-base ms:text-sm">
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>
